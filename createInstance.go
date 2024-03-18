fe88ceae8be096db811602caee284f88c136d246
Ashish Kumar <ashish.kumar85297@gmail.com>
2024-03-18 10:00:03 +0530

    .

--- createInstance.go
+++ createInstance.go
@@ -4,99 +4,297 @@ import (
 	"context"
 	"fmt"
 	"io"
+	"io/ioutil"
+	"os"
+	"os/exec"
+	"os/user"
 
 	compute "cloud.google.com/go/compute/apiv1"
+	"golang.org/x/oauth2/google"
+	computeapi "google.golang.org/api/compute/v1"
 	computepb "google.golang.org/genproto/googleapis/cloud/compute/v1"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 	"google.golang.org/protobuf/proto"
 )
 
-func createInstanceWithFirewall(w io.Writer, projectID, zone, instanceName, machineType, sourceImage, networkName, firewallRuleName string) error {
 
-	ctx := context.Background()
-
-	// Create a compute client
-	computeClient, err := compute.NewInstancesRESTClient(ctx)
-	if err != nil {
-		return fmt.Errorf("NewInstancesRESTClient: %w", err)
-	}
-	defer computeClient.Close()
-
-	// Create a firewall client
+func firewallRuleExists(ctx context.Context, projectID, firewallName string) (bool, error) {
 	firewallClient, err := compute.NewFirewallsRESTClient(ctx)
 	if err != nil {
-		return fmt.Errorf("NewFirewallsRESTClient: %w", err)
+		return false, fmt.Errorf("NewFirewallsRESTClient: %w", err)
 	}
 	defer firewallClient.Close()
 
-	// Define and create the firewall rule
-	firewallRule := &computepb.Firewall{
-		Allowed: []*computepb.Allowed{
-			{
-				IPProtocol: proto.String("tcp"),
-				Ports:      []string{"80", "443"},
-			},
-		},
-		Direction: proto.String(computepb.Firewall_INGRESS.String()),
-		Name:      proto.String(firewallRuleName),
-		TargetTags: []string{
-			"web",
-		},
-		Network:     proto.String(fmt.Sprintf("projects/%s/global/networks/default", projectID)),
-		Description: proto.String("Allowing TCP traffic on port 80 and 443 from Internet."),
+	req := &computepb.GetFirewallRequest{
+		Project: projectID,
+		Firewall: firewallName,
 	}
 
-	// Insert the firewall rule
-		firewallOp, err := firewallClient.Insert(ctx, &computepb.InsertFirewallRequest{
-			Project:          projectID,
-			FirewallResource: firewallRule,
-		})
+	_, err = firewallClient.Get(ctx, req)
+	if err != nil {
+		if status.Code(err) == codes.NotFound {
+			return false, nil
+		}
+		return false, fmt.Errorf("unable to get firewall rule: %w", err)
+	}
+
+	return true, nil
+}
+
+// createInstance sends an instance creation request to the Compute Engine API and waits for it to complete.
+func createInstanceWithFirewall(w io.Writer, projectID, zone, instanceName, machineType, sourceImage, networkName string) error {
+        // projectID := "cloudsec-390404"
+		// zone := "us-east4-c" // Change this to your desired zone
+		// instanceName := "test-vm-inst-5"
+		// machineType := "n1-standard-1" // Change this to your desired machine type
+        // sourceImage := "projects/cloudsec-390404/global/images/image-1"
+        // networkName := "global/networks/default"
+
+
+        tags := []string{"http-server", "https-server"}
+
+        ctx := context.Background()
+        instancesClient, err := compute.NewInstancesRESTClient(ctx)
+        if err != nil {
+                return fmt.Errorf("NewInstancesRESTClient: %w", err)
+        }
+        defer instancesClient.Close()
+
+        // Create a firewall rule to allow ssh traffic
+        firewallClient, err := compute.NewFirewallsRESTClient(ctx)
+        if err != nil {
+        	return fmt.Errorf("NewFirewallsRESTClient: %w", err)
+        }
+        defer firewallClient.Close()
+
+        // Check if the firewall rule already exists
+	firewallExists, err := firewallRuleExists(ctx, projectID, "allow-ssh-ingress-from-iap")
+	if err != nil {
+		return fmt.Errorf("unable to check if firewall rule exists: %w", err)
+	}
+
+        email := "workloadscanserviceaccount@cloudsec-390404.iam.gserviceaccount.com"
+        scope := []string{
+                "https://www.googleapis.com/auth/cloud-platform",
+        }
+        
+
+        if !firewallExists{
+                // Create a firewall rule to allow ssh traffic
+		firewallReq := &computepb.InsertFirewallRequest{
+			Project: projectID,
+			FirewallResource: &computepb.Firewall{
+				Name:         proto.String("allow-ssh-ingress-from-iap"),
+				Network:      proto.String(networkName),
+				SourceRanges: []string{"0.0.0.0/0"},
+				Allowed: []*computepb.Allowed{
+					{
+						IPProtocol: proto.String("tcp"),
+						Ports:      []string{"22"},
+					},
+				},
+
+                                TargetTags: tags,  
+
+
+			},
+                        
+		}
+
+		firewallOp, err := firewallClient.Insert(ctx, firewallReq)
 		if err != nil {
 			return fmt.Errorf("unable to create firewall rule: %w", err)
 		}
 
-		// Wait for the firewall rule operation to complete
-		if err := firewallOp.Wait(ctx); err != nil {
-			return fmt.Errorf("unable to wait for the firewall rule operation: %w", err)
+		if err = firewallOp.Wait(ctx); err != nil {
+			return fmt.Errorf("unable to wait for the firewall operation: %w", err)
 		}
+        } else{
+                // firewallName := "allow-ssh"
+                firewallName := "allow-ssh-ingress-from-iap"
+                // If the firewall rule already exists, update it
+                updatedFirewall := &computepb.Firewall{
+                        Name:         proto.String(firewallName),
+                        Network:      proto.String(networkName),
+                        SourceRanges: []string{"0.0.0.0/0"},
+                        Allowed: []*computepb.Allowed{
+                                {
+                                        IPProtocol: proto.String("tcp"),
+                                        Ports:      []string{"22"},
+                                },
+                        },
+                        TargetTags: tags,
 
-		// Define and create the instance
-		instanceReq := &computepb.InsertInstanceRequest{
-			Project: projectID,
-			Zone:    zone,
-			InstanceResource: &computepb.Instance{
-				Name: proto.String(instanceName),
-				Disks: []*computepb.AttachedDisk{
-					{
-						InitializeParams: &computepb.AttachedDiskInitializeParams{
-							DiskSizeGb:  proto.Int64(10),
-							SourceImage: proto.String(sourceImage),
-						},
-						AutoDelete: proto.Bool(true),
-						Boot:       proto.Bool(true),
-						Type:       proto.String(computepb.AttachedDisk_PERSISTENT.String()),
-					},
-				},
-				MachineType: proto.String(fmt.Sprintf("zones/%s/machineTypes/%s", zone, machineType)),
-				NetworkInterfaces: []*computepb.NetworkInterface{
-					{
-						Name: proto.String(networkName),
-					},
-				},
-			},
-		}
+                }
+                op, err := firewallClient.Update(ctx, &computepb.UpdateFirewallRequest{
+                        Project: projectID,
+                        Firewall: firewallName,
+                        FirewallResource: updatedFirewall,
+                })
+                if err != nil {
+                        return fmt.Errorf("unable to update firewall rule: %w", err)
+                }
 
-		// Insert the instance
-		op, err := computeClient.Insert(ctx, instanceReq)
-		if err != nil {
-			return fmt.Errorf("unable to create instance: %w", err)
-		}
+                if err = op.Wait(ctx); err != nil {
+                        return fmt.Errorf("unable to wait for the firewall operation: %w", err)
+                }
+        }
 
-		// Wait for the instance operation to complete
-		if err = op.Wait(ctx); err != nil {
-			return fmt.Errorf("unable to wait for the operation: %w", err)
-		}
+        req := &computepb.InsertInstanceRequest{
+                Project: projectID,
+                Zone:    zone,
+                InstanceResource: &computepb.Instance{
+                        Name: proto.String(instanceName),
+                        Disks: []*computepb.AttachedDisk{
+                                {
+                                        InitializeParams: &computepb.AttachedDiskInitializeParams{
+                                                DiskSizeGb:  proto.Int64(10),
+                                                SourceImage: proto.String(sourceImage),
+                                        },
+                                        AutoDelete: proto.Bool(true),
+                                        Boot:       proto.Bool(true),
+                                        Type:       proto.String(computepb.AttachedDisk_PERSISTENT.String()),
+                                },
+                        },
+                        MachineType: proto.String(fmt.Sprintf("zones/%s/machineTypes/%s", zone, machineType)),
+                        NetworkInterfaces: []*computepb.NetworkInterface{
+                                {
+                                        Name: proto.String(networkName),
+                                        AccessConfigs: []*computepb.AccessConfig{
+                                                {
+                                                        Name: proto.String("External NAT"),
+                                                        Type: proto.String(computepb.AccessConfig_ONE_TO_ONE_NAT.String()),
+                                                },
+                                        },
+                                        
+                                },
+                        },
+                        Tags: &computepb.Tags{
+                                Items: tags,
+                        },
+                        ServiceAccounts: []*computepb.ServiceAccount{
+                                {
+                                        Email: proto.String(email),
+                                        Scopes: scope,
+                                },
 
-		fmt.Fprintf(w, "Instance created\n")
+                        },
 
-		return nil
+                        
+                },
+        }
+
+        op, err := instancesClient.Insert(ctx, req)
+        if err != nil {
+                return fmt.Errorf("unable to create instance: %w", err)
+        }
+
+        if err = op.Wait(ctx); err != nil {
+                return fmt.Errorf("unable to wait for the operation: %w", err)
+        }
+
+        fmt.Fprintf(w, "Instance created successfully \n")
+
+        return nil
+}
+
+
+
+
+
+
+func generateSSHKeyPair(username string) (privateKey, publicKey []byte, err error) {
+	// Get user's home directory
+	usr, err := user.Current()
+	if err != nil {
+		return nil, nil, fmt.Errorf("failed to get user's home directory: %v", err)
 	}
+	sshDir := usr.HomeDir + "/.ssh/"
+	privKeyPath := sshDir + "gcp_rsa"
+	pubKeyPath := sshDir + "gcp_rsa.pub"
+
+	// Check if the key files exist
+	if _, err := os.Stat(privKeyPath); os.IsNotExist(err) {
+		// Generate RSA private key
+		cmd := exec.Command("ssh-keygen", "-t", "rsa", "-b", "2048", "-f", privKeyPath, "-N", "", "-C", username)
+		err := cmd.Run()
+		if err != nil {
+			return nil, nil, fmt.Errorf("failed to generate private key: %v", err)
+		}
+	}
+
+	// Read the generated private key
+	privKey, err := ioutil.ReadFile(privKeyPath)
+	if err != nil {
+		return nil, nil, fmt.Errorf("failed to read private key: %v", err)
+	}
+
+	// Read the generated public key
+	pubKey, err := ioutil.ReadFile(pubKeyPath)
+	if err != nil {
+		return nil, nil, fmt.Errorf("failed to read public key: %v", err)
+	}
+
+	return privKey, pubKey, nil
+}
+
+
+
+
+
+
+
+func addPublicKeytoInstance(w io.Writer, projectID, zone, instanceName, publicKey string, username string) error {
+    ctx := context.Background()
+
+    // Create a compute service
+    client, err := google.DefaultClient(ctx, computeapi.CloudPlatformScope)
+    if err != nil {
+        return fmt.Errorf("failed to create compute client: %w", err)
+    }
+
+    computeService, err := computeapi.New(client)
+    if err != nil {
+        return fmt.Errorf("failed to create compute service: %w", err)
+    }
+
+    // Get instance info
+    instanceResource := fmt.Sprintf("projects/%s/zones/%s/instances/%s", projectID, zone, instanceName)
+    instanceInfo, err := computeService.Instances.Get(projectID, zone, instanceName).Context(ctx).Do()
+    if err != nil {
+        return fmt.Errorf("failed to get instance info: %v", err)
+    }
+
+    // Print the instance details
+    fmt.Fprintf(w, "Instance details:\n")
+    fmt.Fprintf(w, "Name: %s\n", instanceInfo.Name)
+    fmt.Fprintf(w, "Machine Type: %s\n", instanceInfo.MachineType)
+
+	publicKey= fmt.Sprintf("%s:%s", username, publicKey)
+
+    // Add public key to metadata
+    metadataKey := "ssh-keys"
+    metadataItem := &computeapi.MetadataItems{
+        Key:   metadataKey,
+        Value: &publicKey,
+		
+    }
+    instanceInfo.Metadata.Items = append(instanceInfo.Metadata.Items, metadataItem)
+
+    // Update the instance with new metadata
+    _, err = computeService.Instances.SetMetadata(projectID, zone, instanceName, instanceInfo.Metadata).Context(ctx).Do()
+    if err != nil {
+        return fmt.Errorf("failed to update instance metadata: %v", err)
+    }
+    fmt.Fprintf(w, "Metadata updated\n")
+
+	// Wait for the operation to complete
+	
+	// Get the updated instance info
+	
+
+    fmt.Printf("Public key added to instance metadata %s\n", instanceResource)
+
+    return nil
+}
--- main.go
+++ main.go
@@ -7,11 +7,20 @@ import (
 	"log"
 	"math/rand"
 	"net/http"
+	"os"
 	"os/exec"
+	osUser "os/user"
+	"time"
 
-	functions "cloud.google.com/go/functions/apiv1"
-	"cloud.google.com/go/functions/apiv1/functionspb"
+	// functions "cloud.google.com/go/functions/apiv1"
+
+	// "cloud.google.com/go/functions/apiv1/functionspb"
+
+	functions "cloud.google.com/go/functions/apiv2"
+	"cloud.google.com/go/functions/apiv2/functionspb"
+	"golang.org/x/crypto/ssh"
 	"google.golang.org/api/compute/v1"
+	"google.golang.org/api/option"
 )
 
 func randomInstanceName() string {
@@ -24,28 +33,61 @@ func randomInstanceName() string {
 
 func main() {
 
-	projectID := "cloudsec-390404"
-	// zone := clea"us-central1-a"
-	// instanceName := "trial-instance-1"
-	// machineType := "n1-standard-1"
-	// sourceImage := "projects/debian-cloud/global/images/family/debian-10"
-	// networkName := "global/networks/default"
-	// firewallRuleName := "allow-ssh-dummy"
 
-	vmInstance := "trial-instance-1"
+
+	projectID := "cloudsec-390404"
+
+	vmInstance := "testvm-2"
 
 	
 	Location := "us-east4" // this for cloud functions
-	zone := "us-central1-a" // this for compute engine
-	user := "kumar_236" //  this for the compute engine
+	// zone := "us-central1-a" // this for compute engine
+	zone := "us-east4-c" // this for compute engine
+	user := "ashish" //  this for the compute engine
 
-	functionName := "test-function-1" //hardcoded for now gen 2 function
-	// functionName := "function-3" // gen 1 function
-	// Location = "us-central1" // gen 1 function
+	// functionName := "test-function-1" //hardcoded for now gen 2 function
+	functionName := "function-2" // gen 1 function
+	Location = "us-central1" // gen 1 function
 
+
+	// projectID := "your_project_id"
+	// zone := "europe-central2-b"
+	// instanceName := "your_instance_name"
+
+	machineType := "n1-standard-1"
+	sourceImage := "projects/debian-cloud/global/images/family/debian-10"
+	networkName := "global/networks/default"
+
+
+	privateKey , publicKey, err := generateSSHKeyPair(user)
+	if err != nil {
+		log.Fatalf("Failed to generate SSH key pair: %v", err)
+	}
+
+	fmt.Println("Private Key:", privateKey)
+
+	fmt.Println("Public Key:", publicKey)
+
+
+
+	// Create a new instance
+	createInstanceWithFirewall(os.Stdout, projectID, zone, vmInstance, machineType, sourceImage, networkName)
+
+	time.Sleep(60 * time.Second)
+
+
+
+	
+
+	addPublicKeytoInstance(os.Stdout, projectID, zone, vmInstance, string(publicKey), user)
+
+	// time.Sleep(30 * time.Second)
+
+	// os.WriteFile("privateKey", privateKey, 0644)
+	
 	ctx := context.Background()
 
-	service, err := compute.NewService(ctx)
+	service, err := compute.NewService(ctx, option.WithCredentialsFile("cloudsec-390404-be836ea29934.json"))
 	if err != nil {
 		log.Fatalf("Failed to create Compute Engine service: %v", err)
 	}
@@ -57,69 +99,70 @@ func main() {
 
 	fmt.Println("Instance details:", instance)
 
-	fmt.Printf("Instance details:\n")
-	fmt.Printf("Name: %s\n", instance.Name)
-	fmt.Printf("Machine Type: %s\n", instance.MachineType)
-	fmt.Printf("Status: %s\n", instance.Status)
-	fmt.Printf("Internal IP: %s\n", instance.NetworkInterfaces[0].NetworkIP)
-	fmt.Printf("External IP: %s\n", instance.NetworkInterfaces[0].AccessConfigs[0].NatIP)
+	// fmt.Printf("Instance details:\n")
+	// fmt.Printf("Name: %s\n", instance.Name)
+	// fmt.Printf("Machine Type: %s\n", instance.MachineType)
+	// fmt.Printf("Status: %s\n", instance.Status)
+	// fmt.Printf("Internal IP: %s\n", instance.NetworkInterfaces[0].NetworkIP)
+	// fmt.Printf("External IP: %s\n", instance.NetworkInterfaces[0].AccessConfigs[0].NatIP)
+	external_ip := instance.NetworkInterfaces[0].AccessConfigs[0].NatIP
 
 
-	//take input from user
-	// fmt.Println("Enter the Env gen(1/2):")
-	// var env string
-	// fmt.Scanln (&env)
-	// fmt.Println("Enter the function name:")
 
-	gen_type := GFunctionDescriptionEnv(functionName, Location)
-
-	if gen_type == "GEN_2" {
-
-		copyCloudfunctionGen2(functionName, Location)
-		tranferSourceCode(functionName + ".zip", vmInstance, user, zone)
-
-	} else {
-
-		client, err := functions.NewCloudFunctionsClient(ctx)
-		if err != nil {
-			fmt.Printf("Failed to create client: %v", err)
-			return
-		}
-		defer client.Close()
-
-		function_path := fmt.Sprintf("projects/%s/locations/%s/functions/%s", projectID, Location, functionName)
-
-
-		cloudFunction, err := getCloudFunction(ctx, client, function_path)
-		if err != nil {
-			fmt.Println("Error getting function:", err)
-			return
-		}
-
-		fmt.Println("Cloud FunctioTriggern details:", cloudFunction)
-
-
-		err = downloadFunction(cloudFunction.DownloadUrl, functionName)
-		if err != nil {
-			fmt.Println("Error downloading function:", err)
-			return
-		}
-
-		println("Downloaded function:", functionName + ".zip")
-
-
-		err = tranferSourceCode(functionName + ".zip", vmInstance, user, zone)
-		if err != nil {
-			fmt.Println("Error transferring source code:", err)
-			return
-		}
-
+	client, err := functions.NewFunctionClient(ctx)
+	if err != nil {
+		fmt.Printf("Failed to create client: %v", err)
+		return
 	}
+	defer client.Close()
+
+	function_path := fmt.Sprintf("projects/%s/locations/%s/functions/%s", projectID, Location, functionName)
+
+	cloudFunction, err := getCloudFunction(ctx, client, function_path)
+	if err != nil {
+		fmt.Println("Error getting function:", err)
+		return
+	}
+
+	// println("Cloud Function details:", cloudFunction)
+
+	fmt.Println("Cloud Function details:", cloudFunction)
+
+	err = downloadFunction(cloudFunction.DownloadUrl, functionName)
+	
+
+	usr, err := osUser.Current()
+	if err != nil {
+		fmt.Println("Error getting current user:", err)
+		return
+	}
+
+	sshDir := usr.HomeDir + "/.ssh"
+	privatePathKey := sshDir + "/gcp_rsa"
+
+	err = copySourceCode(functionName + ".zip", external_ip, user, zone, privatePathKey)
+	if err != nil {
+		fmt.Println("Error copying source code:", err)
+		return
+	}
+	os.Remove(functionName + ".zip")
+	println("Source code copied:", functionName + ".zip")
+
+	err = tranferSourceCode(functionName, vmInstance, user, zone, privatePathKey, external_ip, cloudFunction.DownloadUrl)
+
+	if err != nil {
+		fmt.Println("Error transferring source code:", err)
+		return
+	}
+
+	
+	println("Source code removed:", functionName + ".zip  after transfer")
+
 	
 }
 
 
-func getCloudFunction(ctx context.Context, client *functions.CloudFunctionsClient, functionpath string) (*functionspb.GenerateDownloadUrlResponse, error) {
+func getCloudFunction(ctx context.Context, client *functions.FunctionClient, functionpath string) (*functionspb.GenerateDownloadUrlResponse, error) {
 
 	req := &functionspb.GetFunctionRequest{
 		Name: functionpath,
@@ -128,6 +171,14 @@ func getCloudFunction(ctx context.Context, client *functions.CloudFunctionsClien
 	downloadReq := &functionspb.GenerateDownloadUrlRequest{
 		Name: req.Name,
 	}
+
+	
+	info, err := client.GetFunction(ctx, req)
+	if err != nil {
+		return nil, err
+	}
+
+	fmt.Println("Cloud Function details:", info)
 
 	cloudFunction, err := client.GenerateDownloadUrl(ctx, downloadReq)
 	if err != nil {
@@ -162,17 +213,158 @@ func downloadFunction(downloadURL string, functionName string) error {
 }
 
 
+// func tranferSourceCode(localFilePath, vmInstance string, user string, zone string) error {
+
+// 	cmd := exec.Command("gcloud", "compute", "scp", localFilePath, user+"@"+vmInstance+":~/", "--zone", zone)
+//     _, err := cmd.Output()
+
+// 	if err != nil {
+// 		return err
+// 	}
+
+// 	println("Source code transferred to VM instance:", vmInstance)
+// 	return nil
+// }
+
+func copySourceCode(functionName, external_ip, user, zone string, privateKeyPath string) error {
+	
+	scpCmd := fmt.Sprintf("scp -i %s -o StrictHostKeyChecking=no %s %s@%s:~/", privateKeyPath, functionName, user, external_ip)
+    cmd := exec.Command("bash", "-c", scpCmd)
+    if err := cmd.Run(); err != nil {
+        return fmt.Errorf("error copying file to VM instance: %v", err)
+    }
+
+	return nil
+}
 
 
-func tranferSourceCode(localFilePath, vmInstance string, user string, zone string) error {
+func tranferSourceCode(functionName, vmInstance, user, zone, privateKeyPath string, external_ip string, downloadURL string) error {
+	// Create Compute Engine service\\\
+	// computeService, err := compute.NewService(context.Background())
+	// if err != nil {
+	// 	return fmt.Errorf("error creating compute service: %v", err)
+	// }
 
-	cmd := exec.Command("gcloud", "compute", "scp", localFilePath, user+"@"+vmInstance+":~/", "--zone", zone)
-    _, err := cmd.Output()
+	// Get instance info
+	// instanceInfo, err := computeService.Instances.Get("cloudsec-390404", zone, vmInstance).Do()
+	// if err != nil {
+	// 	return fmt.Errorf("error getting instance info: %v", err)
+	// }
 
+	key, err := ioutil.ReadFile(privateKeyPath)
 	if err != nil {
-		return err
+		return fmt.Errorf("error reading private key: %v", err)
 	}
 
-	println("Source code transferred to VM instance:", vmInstance)
+	signer, err := ssh.ParsePrivateKey(key)
+	if err != nil {
+		return fmt.Errorf("error parsing private key: %v", err)
+	}
+
+	config := &ssh.ClientConfig{
+		User: user,
+		Auth: []ssh.AuthMethod{
+			ssh.PublicKeys(signer),
+		},
+		HostKeyCallback: ssh.InsecureIgnoreHostKey(),
+		Timeout:         30 * time.Second,
+	}
+
+	client, err := ssh.Dial("tcp", external_ip+":22", config)
+	if err != nil {
+		return fmt.Errorf("error dialing: %v", err)
+	}
+	defer client.Close()
+
+
+	session, err := client.NewSession()
+	if err != nil {
+		return fmt.Errorf("error creating session: %v", err)
+	}
+	defer session.Close()
+
+	// if err := session.Run("curl -o " + functionName + ".zip " + downloadURL); err != nil {
+	// 	return fmt.Errorf("error downloading function: %v", err)
+	// }
+
+	// defer session.Close()
+	
+	// install unzip
+	session, err = client.NewSession()
+	if err != nil {
+		return fmt.Errorf("error creating session: %v", err)
+	}
+
+	if err := session.Run("sudo apt-get install unzip"); err != nil {
+		return fmt.Errorf("error installing unzip: %v", err)
+	}
+
+	defer session.Close()
+
+	// session, err = client.NewSession()
+	// if err != nil {
+	// 	return fmt.Errorf("error creating session: %v", err)
+	// }
+
+	// if err := session.Run("sudo apt-get install p7zip-full"); err != nil {
+	// 	return fmt.Errorf("error installing 7zip: %v", err)
+	// }
+
+
+	//create directory and install grype
+	session, err = client.NewSession()
+    if err != nil {
+        log.Fatalf("Failed to create session: %v", err)
+    }
+    defer session.Close()
+
+    if err := session.Run("mkdir -p ~/scanner && sudo curl -sSfL https://raw.githubusercontent.com/anchore/grype/main/install.sh | sudo sh -s -- -b /usr/local/bin"); err != nil {
+        log.Fatalf("Error creating directory or installing grype: %v", err)
+    }
+
+
+	//unzip the file in the scanner directory
+
+	session, err = client.NewSession()
+	if err != nil {
+		return fmt.Errorf("error creating session: %v", err)
+	}
+
+	defer session.Close()
+
+	if err := session.Run("unzip -o " + functionName + ".zip -d ~/scanner"); err != nil {
+		return fmt.Errorf("error unzipping file: %v", err)
+	} 
+	println("File unzipped successfully")
+
+
+	//execute the grype command
+
+	session, err = client.NewSession()
+	if err != nil {
+		return fmt.Errorf("error creating session: %v", err)
+	}
+
+	defer session.Close()
+
+	if err := session.Run("grype -v ~/scanner --output json > output.json"); err != nil {
+		return fmt.Errorf("error executing grype command: %v", err)
+	}
+
+	// send the output.json to backend api
+
+	// session, err = client.NewSession()
+	// if err != nil {
+	// 	return fmt.Errorf("error creating session: %v", err)
+	// }
+
+	// if err := session.Run("curl -X POST -H \"Content-Type: application/json\" -d @output.json http://localhost:8080/api/v1/scan"); err != nil {
+
+	// 	return fmt.Errorf("error sending output.json to backend api: %v", err)
+	// }
+    fmt.Println("Commands executed successfully")
+
+
 	return nil
+
 }
